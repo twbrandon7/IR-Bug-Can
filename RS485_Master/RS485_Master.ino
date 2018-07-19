77a2c030d9ef3cc5491fc971c64dc4775c4cd7b1
Li-Xian Chen <b0543017@ems.niu.edu.tw>
2018-07-19 15:19:47 +0800

    將傳輸格式標準化
    Signed-off-by: Li-Xian Chen <b0543017@ems.niu.edu.tw>

--- RS485_Master/RS485_Master.ino
+++ RS485_Master/RS485_Master.ino
@@ -19,6 +19,21 @@
 
 #define PinTransmissionLED         7
 
+typedef struct IR_BUG_CAN_DATA
+{
+  unsigned int time_pass;
+  unsigned int bug_pest; 
+  unsigned int has_data; 
+
+  unsigned int error_count;
+  unsigned int illegal_serial_start_or_end;
+}IrBugCanData;
+
+static const IrBugCanData emptyIrBugCanData{};
+
+void readData(IrBugCanData &);
+unsigned int  hexToDec(String);
+
 /*-----( Declare objects )-----*/
 SoftwareSerial RS485Serial(SSerialRX, SSerialTX); // RX, TX
 
@@ -40,14 +55,57 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(PinTransmissionLED, HIGH);  // Show activity
   
-  if (RS485Serial.available()) {
-    digitalWrite(PinTransmissionLED, LOW);  // Show activity
-    byteReceived = RS485Serial.read();    // Read received byte
-    Serial.println(byteReceived);        // Show on Serial Monitor
-    delay(10);
-    digitalWrite(PinTransmissionLED, HIGH);  // Show activity   
-  }  
+  IrBugCanData data;
+  readData(data);
+  if(data.has_data){
+    Serial.println(String(data.has_data));
+    Serial.println("Time Pass : " + String(data.time_pass));
+    Serial.println("Bug Pest : " + String(data.bug_pest));
+  }
+}
 
+void readData(IrBugCanData &data){
+  data = emptyIrBugCanData;
+  bool haveData = false;
+  String bufferStr;
+  while(RS485Serial.available()){
+    haveData = true;
+    bufferStr += (char)RS485Serial.read();
+    delay(10);
+  }
+  if(haveData){
+    data.has_data++;
+    int len = bufferStr.length() / 4;
+    int dataArr[len];
+    for(int i = 0; i < len; i++){
+      dataArr[i] = hexToDec(bufferStr.substring(i*4, i*4+4));
+    }
+    if(dataArr[0] == -1 && dataArr[len-1] == -2){
+      data.time_pass = dataArr[1];
+      data.bug_pest = dataArr[2]; 
+    }else{
+      data.illegal_serial_start_or_end++;
+      data.error_count++;
+    }
+  }
+}
+
+unsigned int hexToDec(String hexString) {
+  
+  unsigned int decValue = 0;
+  int nextInt;
+  
+  for (int i = 0; i < hexString.length(); i++) {
+    
+    nextInt = int(hexString.charAt(i));
+    if (nextInt >= 48 && nextInt <= 57) nextInt = map(nextInt, 48, 57, 0, 9);
+    if (nextInt >= 65 && nextInt <= 70) nextInt = map(nextInt, 65, 70, 10, 15);
+    if (nextInt >= 97 && nextInt <= 102) nextInt = map(nextInt, 97, 102, 10, 15);
+    nextInt = constrain(nextInt, 0, 15);
+    
+    decValue = (decValue * 16) + nextInt;
+  }
+  
+  return decValue;
 }
--- RS485_Slave/RS485_Slave.ino
+++ RS485_Slave/RS485_Slave.ino
@@ -9,6 +9,19 @@
 
 #include <SoftwareSerial.h>
 
+//Data format
+enum {
+  DATA_START,
+  TIME_PASS,
+  BUG_PEST,
+  DATA_END,
+  //DATA_LEN must be the last one
+  DATA_LEN
+};
+
+#define DATA_START_VAL -1
+#define DATA_END_VAL -2
+
 /*-----( Declare Constants and Pin Numbers )-----*/
 #define PinRX        10  //pin which connected to RO
 #define PinTX        11  //pin which connected to DI
@@ -18,35 +31,81 @@
 #define RS485Transmit    HIGH
 #define RS485Receive     LOW
 
-#define PinTransmissionLED         13
+#define PinTransmissionLED 13
 
+#define PinSimulationButton 7
+
+#define ReportDuration 5000 //millisecond
+
+void snedData(int, int);
+void blinkLED(unsigned char);
+String decToHex(int, byte);
 /*-----( Declare objects )-----*/
 SoftwareSerial RS485Serial(PinRX, PinTX); // RX, TX
 
 /*-----( Declare Variables )-----*/
 int byteSend;
 
+int bugPestCount = 0;
+unsigned long timeLast;
+
 void setup() {
   // Start the built-in serial port, probably to Serial Monitor
   Serial.begin(9600);
   
   pinMode(PinTransmissionLED, OUTPUT);   
-  pinMode(PinTXControl, OUTPUT);  
+  pinMode(PinTXControl, OUTPUT); 
+  pinMode(PinSimulationButton, INPUT); 
   
   digitalWrite(PinTXControl, RS485Transmit);  // Init Transceiver
   
   // Start the software serial port, to another device
   RS485Serial.begin(4800);   // set the data rate 
+  timeLast = millis();
 }
-
 
 void loop() {
-  byteSend = 87;    
-  RS485Serial.write(byteSend); // Send the byte back
-  digitalWrite(PinTransmissionLED, HIGH);  // Show activity    
-  delay(10);     
-  digitalWrite(PinTransmissionLED, LOW);
-  Serial.println("SENT");
-  delay(1000);    
+  if(digitalRead(PinSimulationButton)){
+    bugPestCount++;
+    Serial.println("Pass! current amount : " + String(bugPestCount));
+  }
+
+  if(millis() >= timeLast + ReportDuration){
+    int actualDuration = (int) (millis() - timeLast);
+    
+    snedData(actualDuration, bugPestCount);
+    
+    bugPestCount = 0;
+    Serial.println("SENT");
+    timeLast = millis();
+  }
 }
+
+void snedData(int timePass, int bugPest){
+  String data[DATA_LEN];
+  data[DATA_START] = decToHex(DATA_START_VAL, 4);
+  data[TIME_PASS] = decToHex(timePass, 4);
+  data[BUG_PEST] = decToHex(bugPest, 4);
+  data[DATA_END] = decToHex(DATA_END_VAL, 4);
+  for(int i = 0; i < DATA_LEN; i++){
+    for(int j = 0; j < data[i].length(); j++){
+      RS485Serial.write(data[i].charAt(j));
+      blinkLED(PinTransmissionLED);
+    }
+  }
+}
+
+void blinkLED(unsigned char pin){
+  digitalWrite(pin, LOW);
+  delay(5);
+  digitalWrite(pin, HIGH);
+}
+
+String decToHex(int decValue, byte desiredStringLength) {
+  String hexString = String(decValue, HEX);
+  while (hexString.length() < desiredStringLength) hexString = "0" + hexString;
+  
+  return hexString;
+}
+
 
