9c4eda9341ebeb25099496e07d1589dacd821070
Li-Xian Chen <b0543017@ems.niu.edu.tw>
2018-07-19 17:01:47 +0800

    將接收端的程式碼改寫為Arduino Library
    Signed-off-by: Li-Xian Chen <b0543017@ems.niu.edu.tw>

--- IRBugCanLib/IrBugCanLib.cpp
+++ IRBugCanLib/IrBugCanLib.cpp
@@ -1,6 +1,5 @@
 #include "Arduino.h"
 #include "IrBugCanLib.h"
-#include "SoftwareSerial.h"
 
 IrBugCanLib::IrBugCanLib(SoftwareSerial *serial, unsigned char pinTXControl,
                          unsigned char pinTransmissionLED, int dataRate)
@@ -13,10 +12,10 @@ IrBugCanLib::IrBugCanLib(SoftwareSerial *serial, unsigned char pinTXControl,
 
 void IrBugCanLib::begin()
 {
-    if(_pinTransmissionLED >= 0)
-        pinMode(_pinTransmissionLED, OUTPUT);  
-    pinMode(_pinTXControl, OUTPUT); 
-    _rs485Serial->begin(_dataRate); 
+    if (_pinTransmissionLED >= 0)
+        pinMode(_pinTransmissionLED, OUTPUT);
+    pinMode(_pinTXControl, OUTPUT);
+    _rs485Serial->begin(_dataRate);
 }
 
 void IrBugCanLib::blinkLED()
@@ -27,15 +26,6 @@ void IrBugCanLib::blinkLED()
         delay(5);
         digitalWrite(_pinTransmissionLED, HIGH);
     }
-}
-
-String IrBugCanLib::decToHex(int decValue, byte desiredStringLength)
-{
-    String hexString = String(decValue, HEX);
-    while (hexString.length() < desiredStringLength)
-        hexString = "0" + hexString;
-
-    return hexString;
 }
 
 void IrBugCanLib::snedData(int timePass, int bugPest)
@@ -54,4 +44,68 @@ void IrBugCanLib::snedData(int timePass, int bugPest)
             this->blinkLED();
         }
     }
+}
+
+void IrBugCanLib::readData(IrBugCanData &data)
+{
+    digitalWrite(_pinTXControl, RS485Receive);
+    data = emptyIrBugCanData;
+    bool haveData = false;
+    String bufferStr;
+    while (_rs485Serial->available())
+    {
+        haveData = true;
+        bufferStr += (char)_rs485Serial->read();
+        delay(10);
+    }
+    if (haveData)
+    {
+        data.has_data++;
+        int len = bufferStr.length() / 4;
+        int dataArr[len];
+        for (int i = 0; i < len; i++)
+        {
+            dataArr[i] = this->hexToDec(bufferStr.substring(i * 4, i * 4 + 4));
+        }
+        if (dataArr[0] == -1 && dataArr[len - 1] == -2)
+        {
+            data.time_pass = dataArr[1];
+            data.bug_pest = dataArr[2];
+        }
+        else
+        {
+            data.illegal_serial_start_or_end++;
+            data.error_count++;
+        }
+    }
+}
+
+String IrBugCanLib::decToHex(int decValue, byte desiredStringLength)
+{
+    String hexString = String(decValue, HEX);
+    while (hexString.length() < desiredStringLength)
+        hexString = "0" + hexString;
+
+    return hexString;
+}
+
+unsigned int IrBugCanLib::hexToDec(String hexString)
+{
+    unsigned int decValue = 0;
+    int nextInt;
+
+    for (int i = 0; i < hexString.length(); i++)
+    {
+        nextInt = int(hexString.charAt(i));
+        if (nextInt >= 48 && nextInt <= 57)
+            nextInt = map(nextInt, 48, 57, 0, 9);
+        if (nextInt >= 65 && nextInt <= 70)
+            nextInt = map(nextInt, 65, 70, 10, 15);
+        if (nextInt >= 97 && nextInt <= 102)
+            nextInt = map(nextInt, 97, 102, 10, 15);
+        nextInt = constrain(nextInt, 0, 15);
+
+        decValue = (decValue * 16) + nextInt;
+    }
+    return decValue;
 }
--- IRBugCanLib/IrBugCanLib.h
+++ IRBugCanLib/IrBugCanLib.h
@@ -4,6 +4,9 @@
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 
+#define RS485Transmit    HIGH
+#define RS485Receive     LOW
+
 enum {
   DATA_START,
   TIME_PASS,
@@ -16,8 +19,17 @@ enum {
 #define DATA_START_VAL -1
 #define DATA_END_VAL -2
 
-#define RS485Transmit    HIGH
-#define RS485Receive     LOW
+typedef struct IR_BUG_CAN_DATA
+{
+  unsigned int time_pass;
+  unsigned int bug_pest; 
+  unsigned int has_data; 
+
+  unsigned int error_count;
+  unsigned int illegal_serial_start_or_end;
+} IrBugCanData;
+
+static const IrBugCanData emptyIrBugCanData{};
 
 class IrBugCanLib {
   public:
@@ -25,11 +37,13 @@ class IrBugCanLib {
         unsigned char pinTransmissionLED, int dataRate);
     void begin();
     void snedData(int, int);
+    void readData(IrBugCanData &);
   private:
     int _pinTXControl, _pinTransmissionLED, _dataRate;
 	  SoftwareSerial *_rs485Serial;
     void blinkLED();
     String decToHex(int, byte);
+    unsigned int hexToDec(String);
 };
 
 #endif
