6142909fcb692e877829418bb473aff548e76daa
Li-Xian Chen <twbrandon7@gmail.com>
2018-07-21 16:10:39 +0800

    使用Modbus-Master-Slave-for-Arduino改寫Slave
    Signed-off-by: Li-Xian Chen <twbrandon7@gmail.com>

--- RS485_Slave/RS485_Slave.ino
+++ RS485_Slave/RS485_Slave.ino
@@ -1,13 +1,12 @@
 /* 
- *  IR Bug Can RS485 Module Framework
+ *  IR Bug Can RS485 Modbus RTU
  *  Author : Li-Xian Chen (b0543017@ems.niu.edu.tw)
- *  2018/07/20
+ *  2018/07/21
  *  
- *  Reference : https://github.com/angeloc/simplemodbusng
+ *  Reference : https://github.com/smarmengol/Modbus-Master-Slave-for-Arduino
 */
 
-#include <SoftwareSerial.h>
-#include <SimpleModbusSlaveSoftwareSerial.h>
+#include <ModbusRtu.h>
 
 /*-----( Declare Constants and Pin Numbers )-----*/
 #define PinRX        10  //pin which connected to RO
@@ -15,66 +14,111 @@
 
 #define PinTXControl 2   //RS485 Direction control
 #define PinTransmissionLED 13
-#define CommunicationBaudRate 4800
+#define CommunicationBaudRate 57600
+#define DeviceId 3
+#define DataLength 16
+#define TimeoutArrLen 16
 
 #define PinSimulationButton 7
 
-#define DeviceId 3
-
-enum 
-{     
-  // just add or remove registers and your good to go...
-  // The first register starts at address 0
-  PWM_0, //Preserve for TX
-  PWM_1, //Preserve for RX
-  ADC0, //Preserve for PinTXControl
-  TIME_PASS, //Time Pass
-  BUG_PEST, //Bug Pest
-  TOTAL_ERRORS,
-  // leave this one
-  TOTAL_REGS_SIZE 
-  // total number of registers for function 3 and 16 share the same register array
+enum{
+  HOURS,
+  MINUTES,
+  SECONDS,
+  BUG_PEST,
+  END,
+  DATA_LENGTH
 };
 
-unsigned int holdingRegs[TOTAL_REGS_SIZE]; // function 3 and 16 register array
+void addSecond();
+void addMinute();
+void addHour();
+void resetTimer();
 
-int bugPestCount = 0;
+uint16_t au16data[DATA_LENGTH];
+uint16_t data[DATA_LENGTH];
+
+unsigned int bugPestCount = 0, hours = 0, minutes = 0, seconds = 0;
 unsigned long timeLast;
+bool lastTimeOutState = true;
 
+/**
+ *  Modbus object declaration
+ *  u8id : node id = 0 for master, = 1..247 for slave
+ *  u8serno : serial port (use 0 for Serial, >= 4 for SoftwareSerial)
+ *  u8txenpin : 0 for RS-232 and USB-FTDI 
+ *               or any pin number > 1 for RS-485
+ */
+Modbus slave(DeviceId, 4, PinTXControl); // this is slave @1 and RS-485
 SoftwareSerial rs485Serial(PinRX, PinTX);
 
 void setup() {
-  // Start the built-in serial port, probably to Serial Monitor
-  Serial.begin(9600);
-  modbus_configure(&rs485Serial, CommunicationBaudRate, DeviceId, PinTXControl, TOTAL_REGS_SIZE);
+  au16data[END] = -1;
+  data[END] = -1;
+
+  slave.begin(&rs485Serial, CommunicationBaudRate); // baud-rate at 19200
+  slave.setTimeOut(1500);
+  
   pinMode(PinSimulationButton, INPUT); 
+  Serial.begin(9600);
+  
   timeLast = millis();
 }
 
-bool lastConnectionStatus = false;
-
 void loop() {
+  au16data[HOURS] = hours;
+  au16data[MINUTES] = minutes;
+  au16data[SECONDS] = seconds;
+  au16data[BUG_PEST] = bugPestCount;
+
+  if(slave.getTimeOutState()){
+    memcpy( data, au16data, DATA_LENGTH*sizeof(data[0]) );
+  }
+  
+  slave.poll( data, DATA_LENGTH );
+  
   if(digitalRead(PinSimulationButton)){
     bugPestCount++;
     Serial.println("Pass! current amount : " + String(bugPestCount));
   }
 
-  holdingRegs[TOTAL_ERRORS] = modbus_update(holdingRegs);
-  holdingRegs[TIME_PASS] = 0;
-  holdingRegs[BUG_PEST] = bugPestCount;
-  if(isConnected() && !lastConnectionStatus){
-    Serial.println("Connected");
-  }else if(!isConnected() && lastConnectionStatus){
-    Serial.println("Disconnected");
+  if(millis() - timeLast >= 1000){
+    addSecond();
+    timeLast = millis();
   }
-//  if(millis() >= timeLast + ReportDuration){
-//    int actualDuration = (int) ((millis() - timeLast) / 1000); //minute
-//    
-//    irBugCanLib.snedData(actualDuration, bugPestCount);
-//    
-//    bugPestCount = 0;
-//    Serial.println("SENT");
-//    timeLast = millis();
-//  }
-  lastConnectionStatus = isConnected();
+  
+  if(lastTimeOutState && !slave.getTimeOutState()){ //Connected
+    bugPestCount = 0;
+    resetTimer();
+  }else if(!lastTimeOutState && slave.getTimeOutState()){ //Disconnected
+    //
+  }
+  
+  lastTimeOutState = slave.getTimeOutState();
+}
+
+void addSecond(){
+  if(seconds + 1 >= 60){
+    seconds = 0;
+    addMinute();
+  }else{
+    seconds++;
+  }
+}
+
+void addMinute(){
+  if(minutes + 1 >= 60){
+    minutes = 0;
+    addHour();
+  }else{
+    minutes++;
+  }
+}
+
+void addHour(){
+  hours++;
+}
+
+void resetTimer(){
+  hours = 0; minutes = 0; seconds = 0;
 }
